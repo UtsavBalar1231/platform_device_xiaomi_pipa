89ba1fdb0422ca7ba7eaf8dd4b5f9921e8c67dfa
Yuan Si <do4suki@gmail.com>
2023-09-02 01:12:04 +0800

    pipa: Import QDCM display calibration from hentaiOS
    I am not certain if the exact author to be attributed is @ Dyneteve,
    as the file was extracted by @ KakatkarAkshay from apollo hentaiOS image,
    but they did not release its device tree code.
    
    Co-authored-by: Dyneteve <dyneteve@hentaios.com>
    Change-Id: Idc690f696656d209b0623b4a39cf09314946f7f8
    Signed-off-by: Yuan Si <do4suki@gmail.com>

--- device.mk
+++ device.mk
@@ -144,6 +144,9 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
     ro.surface_flinger.use_content_detection_for_refresh_rate=true
 
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/display/,$(TARGET_COPY_OUT_VENDOR)/etc)
+
 # DRM
 PRODUCT_VENDOR_PROPERTIES += \
     drm.service.enabled=true
