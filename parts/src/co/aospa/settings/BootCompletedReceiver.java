7d1f2d96b9b16c30540762eb56af5a254e495898
Yuan Si <do4suki@gmail.com>
2024-05-02 20:36:30 +0800

    pipa: Move to xiaomi common dolby app
    Change-Id: I67c43d2b3e9a8b8f194869ec01816e6136a445ab
    Signed-off-by: Yuan Si <do4suki@gmail.com>

--- parts/src/co/aospa/settings/BootCompletedReceiver.java
+++ parts/src/co/aospa/settings/BootCompletedReceiver.java
@@ -1,9 +1,18 @@
 /*
  * Copyright (C) 2015 The CyanogenMod Project
  *               2017-2020 The LineageOS Project
- * Copyright (C) 2023 Paranoid Android
  *
- * SPDX-License-Identifier: Apache-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package co.aospa.settings;
@@ -15,7 +24,6 @@ import android.util.Log;
 
 import co.aospa.settings.thermal.ThermalUtils;
 import co.aospa.settings.refreshrate.RefreshUtils;
-import co.aospa.settings.dolby.DolbyUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
@@ -27,6 +35,5 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         ThermalUtils.startService(context);
         RefreshUtils.startService(context);
-        DolbyUtils.getInstance(context).onBootCompleted();
     }
 }
