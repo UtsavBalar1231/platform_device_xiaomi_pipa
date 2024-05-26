c15486c35fa0c0e17aa86e7a1832843493dfdb86
UtsavBalar1231 <utsavbalar1231@gmail.com>
2024-05-26 15:15:02 +0530

    pipa: Remove all unsupported packages
    Pipa does not support sim connectivity, therefore the packages such as
    Google Messages, System Carrier and Telephony overlays, Emergency mode,
    etc. will never be used and will take up the system space.
    
    Change-Id: I237250f160b2608353d5865d082d1d2bf09570e4
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -5,18 +5,28 @@ LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
+   CarrierConfigResCommon_Sys \
    CarrierServices \
    Contacts \
+   DevicePersonalizationPrebuiltPixel2020 \
    Dialer \
-   GoogleDialer \
-   Stk \
+   EmergencyInfoGms \
    Etar \
-   TeleService \
+   GmsTelecommOverlay \
+   GmsTelephonyOverlay \
+   GoogleDialer \
    Jelly \
+   Messages \
+   MmsService \
+   SimAppDialog \
+   Stk \
+   TeleService \
    Telecom \
+   TelecommResCommon_Sys \
    TelephonyProvider \
-   com.google.android.dialer.support \
+   TelephonyResCommon_Sys \
    com.android.phone.common \
+   com.google.android.dialer.support \
    messaging
 
 LOCAL_UNINSTALLABLE_MODULE := true
