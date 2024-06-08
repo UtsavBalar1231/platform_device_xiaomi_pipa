DD3Boh/android_device_xiaomi_pipa

pipa: Add Xiaomi Pen support to Xiaomi Peripheral Manager
Change-Id: I2698b754dd062d581f059456670639c71658f3fc

peripheralmanager/src/org/lineageos/xiaomiperipheralmanager/PenUtils.java
@@ -1,0 +1,70 @@
+/*
+ * Copyright (C) 2023 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.xiaomiperipheralmanager;
+
+import android.content.Context;
+import android.hardware.input.InputManager;
+import android.hardware.input.InputManager.InputDeviceListener;
+import android.os.SystemProperties;
+import android.util.Log;
+import android.view.InputDevice;
+
+public class PenUtils {
+
+    private static final String TAG = "XiaomiPeripheralManagerPenUtils";
+    private static final boolean DEBUG = true;
+
+    private static InputManager mInputManager;
+
+    public static void setup(Context context) {
+        mInputManager = (InputManager) context.getSystemService(Context.INPUT_SERVICE);
+        refreshPenMode();
+        mInputManager.registerInputDeviceListener(mInputDeviceListener, null);
+    }
+
+    private static void enablePenMode() {
+        Log.d(TAG, "enablePenMode: Enable Pen Mode");
+        SystemProperties.set("persist.sys.parts.pen", "18");
+    }
+
+    private static void disablePenMode() {
+        Log.d(TAG, "disablePenMode: Disable Pen Mode");
+        SystemProperties.set("persist.sys.parts.pen", "0");
+    }
+
+    private static void refreshPenMode() {
+        for (int id : mInputManager.getInputDeviceIds()) {
+            if (isDeviceXiaomiPen(id)) {
+                if (DEBUG) Log.d(TAG, "refreshPenMode: Found Xiaomi Pen");
+                enablePenMode();
+                return;
+            }
+        }
+        if (DEBUG) Log.d(TAG, "refreshPenMode: No Xiaomi Pen found");
+        disablePenMode();
+    }
+
+    private static boolean isDeviceXiaomiPen(int id) {
+        InputDevice inputDevice = mInputManager.getInputDevice(id);
+        return inputDevice.getVendorId() == 6421 && inputDevice.getProductId() == 19841;
+    }
+
+    private static InputDeviceListener mInputDeviceListener = new InputDeviceListener() {
+            @Override
+            public void onInputDeviceAdded(int id) {
+                refreshPenMode();
+            }
+            @Override
+            public void onInputDeviceRemoved(int id) {
+                refreshPenMode();
+            }
+            @Override
+            public void onInputDeviceChanged(int id) {
+                refreshPenMode();
+            }
+        };
+}

UtsavBalar1231/platform_device_xiaomi_pipa

Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

peripheralmanager/src/co/aospa/xiaomiperipheralmanager/PenUtils.java
@@ -1,0 +1,70 @@
+/*
+ * Copyright (C) 2023 Paranoid Android
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package co.aospa.xiaomiperipheralmanager;
+
+import android.content.Context;
+import android.hardware.input.InputManager;
+import android.hardware.input.InputManager.InputDeviceListener;
+import android.os.SystemProperties;
+import android.util.Log;
+import android.view.InputDevice;
+
+public class PenUtils {
+
+    private static final String TAG = "XiaomiPeripheralManagerPenUtils";
+    private static final boolean DEBUG = true;
+
+    private static InputManager mInputManager;
+
+    public static void setup(Context context) {
+        mInputManager = (InputManager) context.getSystemService(Context.INPUT_SERVICE);
+        refreshPenMode();
+        mInputManager.registerInputDeviceListener(mInputDeviceListener, null);
+    }
+
+    private static void enablePenMode() {
+        Log.d(TAG, "enablePenMode: Enable Pen Mode");
+        SystemProperties.set("persist.sys.parts.pen", "18");
+    }
+
+    private static void disablePenMode() {
+        Log.d(TAG, "disablePenMode: Disable Pen Mode");
+        SystemProperties.set("persist.sys.parts.pen", "0");
+    }
+
+    private static void refreshPenMode() {
+        for (int id : mInputManager.getInputDeviceIds()) {
+            if (isDeviceXiaomiPen(id)) {
+                if (DEBUG) Log.d(TAG, "refreshPenMode: Found Xiaomi Pen");
+                enablePenMode();
+                return;
+            }
+        }
+        if (DEBUG) Log.d(TAG, "refreshPenMode: No Xiaomi Pen found");
+        disablePenMode();
+    }
+
+    private static boolean isDeviceXiaomiPen(int id) {
+        InputDevice inputDevice = mInputManager.getInputDevice(id);
+        return inputDevice.getVendorId() == 6421 && inputDevice.getProductId() == 19841;
+    }
+
+    private static InputDeviceListener mInputDeviceListener = new InputDeviceListener() {
+            @Override
+            public void onInputDeviceAdded(int id) {
+                refreshPenMode();
+            }
+            @Override
+            public void onInputDeviceRemoved(int id) {
+                refreshPenMode();
+            }
+            @Override
+            public void onInputDeviceChanged(int id) {
+                refreshPenMode();
+            }
+        };
+}