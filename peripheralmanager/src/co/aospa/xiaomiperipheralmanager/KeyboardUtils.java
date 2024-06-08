DD3Boh/android_device_xiaomi_pipa

pipa: Add Xiaomi Peripheral Manager
 * right now keyboardutils just does disable the keyboard input device after boot

Change-Id: I7020b043e9e643d125e9641b18f3f0e71d8343b1

peripheralmanager/src/org/lineageos/xiaomiperipheralmanager/KeyboardUtils.java
@@ -1,0 +1,48 @@
+/*
+ * Copyright (C) 2023 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.xiaomiperipheralmanager;
+
+import android.content.Context;
+import android.hardware.input.InputManager;
+import android.util.Log;
+import android.view.InputDevice;
+
+public class KeyboardUtils {
+
+    private static final String TAG = "XiaomiPeripheralManagerKeyboardUtils";
+    private static final boolean DEBUG = true;
+
+    private static InputManager mInputManager;
+
+    public static void setup(Context context) {
+        if (mInputManager == null) {
+            mInputManager = (InputManager) context.getSystemService(Context.INPUT_SERVICE);
+        }
+        setKeyboardEnabled(false);
+    }
+
+    public static void setKeyboardEnabled(boolean enabled) {
+        if (DEBUG) Log.d(TAG, "setKeyboardEnabled: " + enabled);
+        for (int id : mInputManager.getInputDeviceIds()) {
+            if (isDeviceXiaomiKeyboard(id)) {
+                if (DEBUG) Log.d(TAG, "setKeyboardEnabled: Found Xiaomi Keyboard with id: " + id);
+                if (enabled) {
+                    if (DEBUG) Log.d(TAG, "setKeyboardEnabled: Enabling Xiaomi Keyboard");
+                    mInputManager.enableInputDevice(id);
+                } else {
+                    if (DEBUG) Log.d(TAG, "setKeyboardEnabled: Disabling Xiaomi Keyboard");
+                    mInputManager.disableInputDevice(id);
+                }
+            }
+        }
+    }
+
+    private static boolean isDeviceXiaomiKeyboard(int id) {
+        InputDevice inputDevice = mInputManager.getInputDevice(id);
+        return inputDevice.getVendorId() == 5593 && inputDevice.getProductId() == 163;
+    }
+}

UtsavBalar1231/platform_device_xiaomi_pipa

Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

peripheralmanager/src/co/aospa/xiaomiperipheralmanager/KeyboardUtils.java
@@ -1,0 +1,48 @@
+/*
+ * Copyright (C) 2023 Paranoid Android
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package co.aospa.xiaomiperipheralmanager;
+
+import android.content.Context;
+import android.hardware.input.InputManager;
+import android.util.Log;
+import android.view.InputDevice;
+
+public class KeyboardUtils {
+
+    private static final String TAG = "XiaomiPeripheralManagerKeyboardUtils";
+    private static final boolean DEBUG = true;
+
+    private static InputManager mInputManager;
+
+    public static void setup(Context context) {
+        if (mInputManager == null) {
+            mInputManager = (InputManager) context.getSystemService(Context.INPUT_SERVICE);
+        }
+        setKeyboardEnabled(false);
+    }
+
+    public static void setKeyboardEnabled(boolean enabled) {
+        if (DEBUG) Log.d(TAG, "setKeyboardEnabled: " + enabled);
+        for (int id : mInputManager.getInputDeviceIds()) {
+            if (isDeviceXiaomiKeyboard(id)) {
+                if (DEBUG) Log.d(TAG, "setKeyboardEnabled: Found Xiaomi Keyboard with id: " + id);
+                if (enabled) {
+                    if (DEBUG) Log.d(TAG, "setKeyboardEnabled: Enabling Xiaomi Keyboard");
+                    mInputManager.enableInputDevice(id);
+                } else {
+                    if (DEBUG) Log.d(TAG, "setKeyboardEnabled: Disabling Xiaomi Keyboard");
+                    mInputManager.disableInputDevice(id);
+                }
+            }
+        }
+    }
+
+    private static boolean isDeviceXiaomiKeyboard(int id) {
+        InputDevice inputDevice = mInputManager.getInputDevice(id);
+        return inputDevice.getVendorId() == 5593 && inputDevice.getProductId() == 163;
+    }
+}